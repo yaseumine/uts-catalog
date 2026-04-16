5b0afb32e72408822bfe90fa318bb46f533df6f2
yaseumine <yasminmaharani123@gmail.com>
2026-04-16 22:10:40 +0700

    feat : menambahkan dio clients

---
+++ lib/core/services/dio_clients.dart
@@ -0,0 +1,59 @@
+import 'package:catalog/core/constants/api_constrants.dart';
+import 'package:catalog/core/services/secure_storage.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+class DioClient {
+  static Dio? _instance;
+  static Dio get instance {
+    _instance ??= _createDio(); // Singleton pattern
+    return _instance!;
+  }
+
+  static Dio _createDio() {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: Duration(milliseconds: ApiConstants.connectTimeout),
+        receiveTimeout: Duration(milliseconds: ApiConstants.receiveTimeout),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
+
+    // Interceptor 1: Logging
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          debugPrint('[REQUEST] ${options.method} ${options.path}');
+          handler.next(options);
+        },
+        onResponse: (response, handler) {
+          debugPrint('[RESPONSE] ${response.statusCode}');
+          handler.next(response);
+        },
+        onError: (error, handler) async {
+          debugPrint('[ERROR] ${error.response?.statusCode}');
+          if (error.response?.statusCode == 401) {
+            await SecureStorageService.clearAll(); // Auto logout
+          }
+          handler.next(error);
+        },
+      ),
+    );
+
+    // Interceptor 2: Auto-inject Bearer Token
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final token = await SecureStorageService.getToken();
+          if (token != null) {
+            options.headers['Authorization'] = 'Bearer $token';
+          }
+          handler.next(options);
+        },
+      ),
+    );
+
+    return dio;
+  }
+}
