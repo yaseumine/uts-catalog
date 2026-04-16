054d03c4306f695f4257381aea4c3e93016e49e5
yaseumine <yasminmaharani123@gmail.com>
2026-04-16 21:38:14 +0700

    feat : menambahkan kode api contransts
    supaya saat backend ganti ga perlu ubah banyak file

--- lib/core/constants/api_constrants.dart
+++ lib/core/constants/api_constrants.dart
@@ -1,5 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.0.28:8080/v1';
+  static const String baseUrl = 'https://api.example.com';
+  // ntar aku ambil ini dari ip laptop aku
 
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
