1c034bb5e7558d03967b210303c664632ef3026a
yaseumine <yasminmaharani123@gmail.com>
2026-04-20 23:49:39 +0700

    fix : prodyct models
    fix karena error

--- lib/features/dashboard/data/models/product_models.dart
+++ lib/features/dashboard/data/models/product_models.dart
@@ -3,36 +3,26 @@ import 'package:equatable/equatable.dart';
 class ProductModel extends Equatable {
   final int id;
   final String name;
-  final String description;
   final double price;
-  final int stock;
-  final String category;
   final String imageUrl;
-  final bool isActive;
+  final String category;
 
   const ProductModel({
     required this.id,
     required this.name,
-    required this.description,
     required this.price,
-    required this.stock,
-    required this.category,
     required this.imageUrl,
-    required this.isActive,
+    required this.category,
   });
 
-  // fromJson: mengubah Map<String, dynamic> (JSON) → ProductModel
   factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
-    id: json['id'] as int? ?? json['ID'] as int? ?? 0,
-    name: json['name'] as String? ?? '',
-    description: json['description'] as String? ?? '',
-    price: (json['price'] as num?)?.toDouble() ?? 0.0,
-    stock: json['stock'] as int? ?? 0,
-    category: json['category'] as String? ?? '',
-    imageUrl: json['image_url'] as String? ?? '',
-    isActive: json['is_active'] as bool? ?? true,
+    id: json['id'] as int,
+    name: json['name'] as String,
+    price: (json['price'] as num).toDouble(),
+    imageUrl: json['image_url'] as String,
+    category: json['category'] as String,
   );
 
   @override
-  List<Object?> get props => [id, name, price, category];
+  List<Object?> get props => [id, name, price];
 }
