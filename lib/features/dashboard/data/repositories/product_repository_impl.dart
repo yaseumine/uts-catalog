224aaa0c15f43579c5c1be00047d8819f040aefe
yaseumine <yasminmaharani123@gmail.com>
2026-04-21 00:05:12 +0700

    feat: product repo impl
    bikin prodyct impl

--- lib/features/dashboard/data/repositories/product_repository_impl.dart
+++ lib/features/dashboard/data/repositories/product_repository_impl.dart
@@ -1,10 +1,29 @@
+import 'package:catalog/core/constants/api_constrants.dart';
+import 'package:catalog/core/services/dio_clients.dart';
 import 'package:catalog/features/dashboard/data/models/product_models.dart';
+import 'package:catalog/features/dashboard/domain/repositories/product_repository.dart';
 
-abstract class ProductRepository {
+class ProductRepositoryImpl implements ProductRepository {
+  @override
   Future<List<ProductModel>> getProducts({
     int page = 1,
     int limit = 10,
     String? category,
-  });
-  Future<ProductModel> getProductById(int id);
+  }) async {
+    final response = await DioClient.instance.get(
+      ApiConstants.products,
+      queryParameters: {'page': page, 'limit': limit, 'category': category},
+    );
+
+    final List<dynamic> data = response.data['data'];
+    return data.map((e) => ProductModel.fromJson(e)).toList();
+  }
+
+  @override
+  Future<ProductModel> getProductById(int id) async {
+    final response = await DioClient.instance.get(
+      '${ApiConstants.products}/$id',
+    );
+    return ProductModel.fromJson(response.data['data']);
+  }
 }
