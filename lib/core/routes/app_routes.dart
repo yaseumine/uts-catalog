1ab96aa0b72fc909c6fead18942cba9d1b7a1258
yaseumine <yasminmaharani123@gmail.com>
2026-04-20 23:55:39 +0700

    feat : app routes
    membuat app routes

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,45 @@
+import 'package:catalog/features/auth/presentation/pages/login_page.dart';
+import 'package:catalog/features/auth/presentation/pages/register_page.dart';
+import 'package:catalog/features/auth/presentation/pages/verify_email_page.dart';
+import 'package:catalog/features/auth/presentation/providers/auth_provider.dart';
+import 'package:catalog/features/dashboard/presentation/pages/dashboard_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AppRouter {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String verifyEmail = '/verify-email';
+  static const String dashboard = '/dashboard';
+
+  static Map<String, WidgetBuilder> get routes => {
+    // splash: (_) => const SplashPage(),
+    login: (_) => const LoginPage(),
+    register: (_) => const RegisterPage(),
+    verifyEmail: (_) => const VerifyEmailPage(),
+    dashboard: (_) => const AuthGuard(child: DashboardPage()),
+  };
+}
+
+// Bungkus halaman yang butuh autentikasi dengan AuthGuard
+class AuthGuard extends StatelessWidget {
+  final Widget child;
+  const AuthGuard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final status = context.watch<AuthProvider>().status;
+
+    return switch (status) {
+      AuthStatus.authenticated => child, // Lanjut ke halaman
+      AuthStatus.emailNotVerified =>
+        const VerifyEmailPage(), // Redirect verifikasi
+      _ => const LoginPage(), // Redirect login
+    };
+  }
+}
+
+// Penggunaan di routes:
+// dashboard: (_) => const AuthGuard(child: DashboardPage())
+//            ↑ DashboardPage HANYA muncul jika status = authenticated
