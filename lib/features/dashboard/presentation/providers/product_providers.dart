d98e35b9458dc8d69634fa6be9c7c395063ae8d7
yaseumine <yasminmaharani123@gmail.com>
2026-04-20 23:48:12 +0700

    feat : product providers
    membuat product providers

---
+++ lib/features/dashboard/presentation/providers/product_providers.dart
@@ -0,0 +1,37 @@
+import 'package:catalog/core/constants/api_constrants.dart';
+import 'package:catalog/core/services/dio_clients.dart';
+import 'package:catalog/features/dashboard/data/models/product_models.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+enum ProductStatus { initial, loading, loaded, error }
+
+class ProductProvider extends ChangeNotifier {
+  ProductStatus _status = ProductStatus.initial;
+  List<ProductModel> _products = [];
+  String? _error;
+
+  Future<void> fetchProducts() async {
+    // 1. Set status loading → UI tampilkan spinner
+    _status = ProductStatus.loading;
+    notifyListeners();
+
+    try {
+      // 2. Hit API — token otomatis di-inject interceptor
+      final response = await DioClient.instance.get(ApiConstants.products);
+
+      // 3. Parse JSON array menjadi List<ProductModel>
+      final List<dynamic> data = response.data['data'];
+      _products = data.map((e) => ProductModel.fromJson(e)).toList();
+
+      // 4. Set status loaded → UI tampilkan grid produk
+      _status = ProductStatus.loaded;
+    } on DioException catch (e) {
+      // 5. Jika error → set status error → UI tampilkan pesan
+      _error = e.response?.data['message'] ?? 'Gagal memuat produk';
+      _status = ProductStatus.error;
+    }
+
+    notifyListeners(); // Beritahu semua widget yang listen
+  }
+}
