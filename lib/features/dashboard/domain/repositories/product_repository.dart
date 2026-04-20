1cf240e1f57fe94aac3e9a0552409e8fbf721a56
yaseumine <yasminmaharani123@gmail.com>
2026-04-21 00:03:31 +0700

    feat : product repositories
    membuat product repository

--- lib/features/dashboard/domain/repositories/product_repository.dart
+++ lib/features/dashboard/domain/repositories/product_repository.dart
@@ -1,1 +1,10 @@
+import 'package:catalog/features/dashboard/data/models/product_models.dart';
 
+abstract class ProductRepository {
+  Future<List<ProductModel>> getProducts({
+    int page = 1,
+    int limit = 10,
+    String? category,
+  });
+  Future<ProductModel> getProductById(int id);
+}
